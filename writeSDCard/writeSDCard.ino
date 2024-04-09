09b536e4f17f4bc4ebe51029db1b3eb71d35cfc0
bmyaye <114804236+bmyaye@users.noreply.github.com>
2024-04-09 14:15:48 +0700

    write data into micro SD card using esp32

---
+++ writeSDCard/writeSDCard.ino
@@ -0,0 +1,40 @@
+#include <SD.h>
+#include <SPI.h>
+
+File file;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialize SD card
+  if (!SD.begin()) {
+    Serial.println("SD card initialization failed");
+    return;
+  }
+  
+  Serial.println("Type your name, surname, age, birthdate, phone and press Enter:");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String data = Serial.readStringUntil('\n'); // Read a line from Serial Monitor
+    
+    // Open file in append mode
+    file = SD.open("/testData.csv", FILE_APPEND);
+    if (!file) {
+      Serial.println("Failed to open file for writing");
+      return;
+    }
+    
+    // If file is not empty, add a newline before writing new data
+    if (file.size() > 0) {
+      file.println();
+    }
+    
+    // Write data to the file
+    file.print(data);
+    file.close(); // Close the file
+    
+    Serial.println("Data written to file successfully!");
+  }
+}
